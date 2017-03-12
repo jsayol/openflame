58733f4d2d9641a87a57dd6173df7904f3b76714
Josep Sayol <josep.sayol@gmail.com>
2017-03-12 01:59:00 +0100

    fix(database): export necessary symbols

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,17 +1,3 @@
-// import { Openflame } from '@openflame/core';
-// import { Database } from './database';
-//
-// declare module '@openflame/core' {
-//   interface Openflame {
-//     database: () => Database;
-//   }
-// }
-//
-// Openflame.prototype.database = function (): Database {
-//   if (!this._instances['database'])
-//     this._instances['database'] = new Database(this);
-//
-//   return <Database>this._instances['database'];
-// };
-
 export * from './database';
+export * from './data-snapshot';
+export * from './reference';
