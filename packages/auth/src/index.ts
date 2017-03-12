c9b99619a0a972c3dd3945c6c89b12c348d03759
Josep Sayol <josep.sayol@gmail.com>
2017-03-12 02:00:24 +0100

    chore(auth): cleanup

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,19 +1,2 @@
-// import { Openflame } from '@openflame/core/src/core';
-// import { Auth } from './auth';
-//
-//
-// declare module '@openflame/core/src/core' {
-//   interface Openflame {
-//     auth: () => Auth;
-//   }
-// }
-//
-// Openflame.prototype.auth = function (): Auth {
-//   if (!this._instances['auth'])
-//     this._instances['auth'] = new Auth(this);
-//
-//   return <Auth>this._instances['auth'];
-// };
-
 export * from './auth';
 export * from './user';
