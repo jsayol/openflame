93321746b78c611e9943256a906f99c706bd22d0
Josep Sayol <josep.sayol@gmail.com>
2017-03-18 11:01:31 +0100

    feat(core): expose the Openflame class on the root scope

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,32 @@
+import { Openflame } from './core';
+
+declare let global: NodeJS.Global;
+
+declare module NodeJS {
+  interface Global {
+    window: any;
+    global: any;
+  }
+}
+
+/**
+ * window: browser in DOM main thread
+ * self: browser in WebWorker
+ * global: Node.js/other
+ *
+ * Adapted from: https://github.com/ReactiveX/rxjs/blob/a3823d2bc7d1bd4d22944c53b27c2af682cddb2a/src/util/root.ts
+ */
+export const root: any = (
+  typeof window == 'object' && window.window === window && window
+  || typeof self == 'object' && self.self === self && self
+  || typeof global == 'object' && global.global === global && global
+);
+
+if (root) {
+  root['Openflame'] = Openflame;
+}
+
+
+
+
 export * from './core';
